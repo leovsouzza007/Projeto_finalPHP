d1e2ca022212cdd43d97cc967e51897118155ab4
leovsouzza007 <leonardovsouzza@gmail.com>
2025-11-19 09:49:42 -0300

    colocando aspas no password (formulario)

--- forms/form_cadastro.php
+++ forms/form_cadastro.php
@@ -11,7 +11,7 @@
         <input type="text" name="nome" id="nome" required><br>
         
         <label for="senha">Senha:</label>
-        <input type=password" name="senha" id="senha" required><br>
+        <input type="password" name="senha" id="senha" required><br>
 
         <label for="email">Email:</label>
         <input type="email" name="email" id="email" required><br>
